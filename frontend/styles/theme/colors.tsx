2c271dc433749f5e26f91809042cbb0ceedc6e01
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2025-03-11 12:03:28 +0900

    feat: define base styles and typography settings #3

---
+++ frontend/styles/theme/colors.tsx
@@ -0,0 +1,13 @@
+const colors = {
+    sideBar: '#F0F4F8',
+    Button: '#F5F5F5',
+    normal_text: '#000000',
+    model_name_text: '#484B4A',
+    Text_input: '#FCFFF7',
+    chat_area: {
+        User: '#C0F177',
+        Agent: '#BCC8FC',
+    }
+}
+
+export default colors
---
+++ frontend/styles/theme/fontSize.tsx
@@ -0,0 +1,8 @@
+const fontSize = {
+    Small: '1rem',  // 16px
+    Medium: '1.25rem', // 20px
+    Large: '1.5rem', //24px
+    ExtraLarge: '2rem', // 32px
+}
+
+export default fontSize

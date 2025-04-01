ca5bcf2a4312da149686cbcf9614982d689560d0
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2025-04-01 02:39:39 +0000

    feat: Send and receive microphone audio to and from the backend #5

--- frontend/components/organisms/MainChatLayout/index.tsx
+++ frontend/components/organisms/MainChatLayout/index.tsx
@@ -3,12 +3,15 @@ import styled from "styled-components";
 import AIPanel from "@components/organisms/AIPanel";
 import UserPanel from "@components/organisms/UserPanel";
 import Flex from "@components/styles/Flex";
+import { AudioChatProvider } from "@features/chat/context/AudioChatContext";
 
 const MainChatLayout = () => {
     return (
         <Flex $width="100%" $height="100vh">
             <AIPanel />
-            <UserPanel />
+            <AudioChatProvider>
+                <UserPanel />
+            </AudioChatProvider>
         </Flex>
     );
 };

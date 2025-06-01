ac2673695ce6a2eba4cde69eba297bdba4c3394a
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2025-06-01 13:31:55 +0900

    feat(backend): add model selector #19

---
+++ backend/app/api/utils/model_selector.py
@@ -0,0 +1,33 @@
+# This module provides functions to select the appropriate instances
+# based on the model name for TTS, transcription, and multimodal response.
+
+import asyncio
+
+from api.modules.transcribers.whisper_transcriber import (
+    WhisperSimpleTranscriber,
+)
+from services.openai_chat import get_chat_response, get_multimodal_response
+from services.tts_generator import TTSGenerator
+
+
+def get_transcriber_instance(model_name: str):
+    if model_name.startswith("rumina-m1"):
+        whisper_transcriber = WhisperSimpleTranscriber()
+        return whisper_transcriber
+    else:
+        raise ValueError(f"Unsupported model: {model_name}")
+
+
+def get_multimodal_response_func(model_name: str):
+    if model_name.startswith("rumina-m1"):
+        return get_multimodal_response
+    else:
+        raise ValueError(f"Unsupported model: {model_name}")
+
+
+def get_tts_instance(model_name: str):
+    if model_name.startswith("rumina-m1"):
+        tts_instance = TTSGenerator()
+        return tts_instance
+    else:
+        raise ValueError(f"Unsupported model: {model_name}")
